72c7b517b0a0fe707ae85537c9d29d8766ee1829
Mannuel1802 <131636387+Mannuel1802@users.noreply.github.com>
2023-08-29 21:38:37 -0600

    Update Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -1,1 +1,13 @@
-print("hola mundo")
+import datetime
+class Servicio:
+  def _init_(self, nombre, costo):
+    self.nombre=nombre
+    self.costo=costo
+    class Nota:
+      folio_counter=1
+      def _init_(self, cliente, servicios):
+        self.folio=nota.folio_counter
+        Nota.folio_counter +=1
+        self.cliente=cliente
+        self.servicios=servicios
+        self.monto_pagar=sum(servicio.costo
